98971f263aa48d96091aa624ef746ce881a1c903
Dmitry.Sorochan <d1d1d1@inbox.ru>
2019-12-13 14:05:07 +0400

    Excel Export Fix

--- app/Export.php
+++ app/Export.php
@@ -2,40 +2,33 @@
 
 namespace App;
 
-use Illuminate\Contracts\View\View;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
-use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
-class Export implements FromView, ShouldAutoSize, WithColumnFormatting
+class Export implements FromArray, WithColumnFormatting
 {
     private $data;
-
+	
     public function __construct($data)
     {
         $this->data = $data;
     }
-
-    public function view(): View
-    {
-        return view('exports.xlsx', [
-            'data' => $this->data
-        ]);
-    }
-
+	
+	public function array(): array
+	{
+		return $this->data;
+	}
+	
 	/**
      * @return array
      */
     public function columnFormats(): array
-    {    	$data = $this->data;
-
-		$arr = [];
-
-		foreach (range('A','Z') as $v)
-		{			$arr[$v] = NumberFormat::FORMAT_TEXT;			$arr['A'.$v] = NumberFormat::FORMAT_TEXT;
-		}
-
-        return $arr;
+    {
+		return [
+			'E' => NumberFormat::FORMAT_NUMBER,
+			'F' => NumberFormat::FORMAT_NUMBER,
+			'G' => NumberFormat::FORMAT_NUMBER,
+		];
     }
 }
